cacb4f70be839dc821ab056dffe31f878c401e7a
stelse99 <stelse99@gmail.com>
2021-07-28 20:34:13 +0900

    UserService 작성

--- src/main/java/com/scp/blog/service/UserService.java
+++ src/main/java/com/scp/blog/service/UserService.java
@@ -1,5 +1,7 @@
 package com.scp.blog.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Transactional
 	public int 회원가입(User user) {
 		
 		try {
