dbfb861c2ab1a76ae438011e90aac962250115f7
stelse99 <stelse99@gmail.com>
2021-08-02 21:41:27 +0900

    security hash 로 password save 처리

--- src/main/java/com/scp/blog/config/SecurityConfig.java
+++ src/main/java/com/scp/blog/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.scp.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration // 빈등록(IoC관리)
 @EnableWebSecurity // 시큐리티 필터등록=스프링 시큐리티가 활성화 되어 있는데 어떤설정을 해당 파일에서 하겠다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정주소로 접근을 하면 권한 및 인증을 미리 체크 하겠다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Bean
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() //csrf token 비활성화(테스트시 걸어주는게 좋다.)
 			.authorizeRequests()
 				//.antMatchers("/auth/loginForm", "/auth/joinform")
-				.antMatchers("/auth/**")
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
--- src/main/java/com/scp/blog/service/UserService.java
+++ src/main/java/com/scp/blog/service/UserService.java
@@ -3,19 +3,28 @@ package com.scp.blog.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.scp.blog.model.RoleType;
 import com.scp.blog.model.User;
 import com.scp.blog.repository.UserRepository;
 
 @Service
 public class UserService {
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Transactional
 	public void 회원가입(User user) {
+		String rawPassword = user.getPassword(); // 123 원문
+		String encPassword = encoder.encode(rawPassword); // 해쉬
+		
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 	
