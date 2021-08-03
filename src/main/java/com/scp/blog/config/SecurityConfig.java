c4dc58edfbe2957c7d7ae9f662410a08c17c4c5c
stelse99 <stelse99@gmail.com>
2021-08-03 21:18:47 +0900

    AuthenticationPrincipal

--- src/main/java/com/scp/blog/config/SecurityConfig.java
+++ src/main/java/com/scp/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.scp.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,10 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.scp.blog.config.auth.PrincipalDetail;
+import com.scp.blog.config.auth.PrincipalDetailService;
+
 @Configuration // 빈등록(IoC관리)
 @EnableWebSecurity // 시큐리티 필터등록=스프링 시큐리티가 활성화 되어 있는데 어떤설정을 해당 파일에서 하겠다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정주소로 접근을 하면 권한 및 인증을 미리 체크 하겠다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
 	
 	@Bean
 	public BCryptPasswordEncoder encodePWD() {
@@ -26,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.userDetailsService(null)
+			.userDetailsService(principalDetailService)
 			.passwordEncoder(encodePWD())
 			;
 	}
--- src/main/java/com/scp/blog/controller/BoardController.java
+++ src/main/java/com/scp/blog/controller/BoardController.java
@@ -1,14 +1,18 @@
 package com.scp.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.scp.blog.config.auth.PrincipalDetail;
 
 @Controller
 public class BoardController {
 	
 	@GetMapping({"","/"})
-	public String index() {
+	public String index(@AuthenticationPrincipal PrincipalDetail principal) { // 컨트롤러에서는 세션을 어떻게 찾는지?
 		// /WEB-INF/views/index.jsp
+		System.out.println("로그인 사용자 id:"+principal);
 		return "index";
 	}
 
