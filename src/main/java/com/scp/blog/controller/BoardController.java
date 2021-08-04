4e65f5ca5d9bd89f6547988e6c28421e12ce2e60
stelse99 <stelse99@gmail.com>
2021-08-04 22:33:11 +0900

    BoardController

--- src/main/java/com/scp/blog/controller/BoardController.java
+++ src/main/java/com/scp/blog/controller/BoardController.java
@@ -12,7 +12,7 @@ public class BoardController {
 	@GetMapping({"","/"})
 	public String index(@AuthenticationPrincipal PrincipalDetail principal) { // 컨트롤러에서는 세션을 어떻게 찾는지?
 		// /WEB-INF/views/index.jsp
-		System.out.println("로그인 사용자 id:"+principal);
+		// System.out.println("로그인 사용자 id:"+principal);
 		return "index";
 	}
 
